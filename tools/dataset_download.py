598af126e5d40e7ce79b7338c9247f9eb2416eb5
ashayaan <a.shayaan@gmail.com>
2018-10-30 15:27:25 +0530

    Donwloading only rock songs

--- tools/dataset_download.py
+++ tools/dataset_download.py
@@ -8,76 +8,85 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 
 def download_file(link, save_file_path):
-    """
-    takes in a URL and the output file save path downloads said file chunk by chunk with a progress bar to display
-    :param link: link to download
-    :param save_file_path: path to save the file in
-    :return:
-    """
-    r = requests.get(link, headers=headers, stream=True)
-    file_size = int(r.headers['Content-Length'])
-    chunk_size = 1024
-    num_bars = int(file_size / chunk_size)
+	"""
+	takes in a URL and the output file save path downloads said file chunk by chunk with a progress bar to display
+	:param link: link to download
+	:param save_file_path: path to save the file in
+	:return:
+	"""
+	r = requests.get(link, headers=headers, stream=True)
+	file_size = int(r.headers['Content-Length'])
+	chunk_size = 1024
+	num_bars = int(file_size / chunk_size)
 
-    with open(save_file_path, "wb") as code:
-        for chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=save_file_path,
-                          leave=True):
-            code.write(chunk)
+	with open(save_file_path, "wb") as code:
+		for chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=save_file_path,
+						  leave=True):
+			code.write(chunk)
 
 
 def download_songs_from_cambridge_mt(download_url, save_dir):
-    """
-    This function downloads the songs from cambridge_mt - Practice Audio Files from the book - Mixing Secrets by Mike Senior
-    :param download_url: the url to download the songs from
-    :param save_dir: directory of dataset_raw - subdirectories will be created for every song
-    :return:
-    """
-    # create save_dir if path does not exist
-    save_dir_abs = os.path.abspath(save_dir)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+	"""
+	This function downloads the songs from cambridge_mt - Practice Audio Files from the book - Mixing Secrets by Mike Senior
+	:param download_url: the url to download the songs from
+	:param save_dir: directory of dataset_raw - subdirectories will be created for every song
+	:return:
+	"""
+	# create save_dir if path does not exist
+	save_dir_abs = os.path.abspath(save_dir)
+	if not os.path.exists(save_dir):
+		os.makedirs(save_dir)
 
-    page = requests.get(download_url)
-    soup = BeautifulSoup(page.text, "lxml")
+	page = requests.get(download_url)
+	soup = BeautifulSoup(page.text, "lxml")
 
-    count = 1
-    for song in soup.find_all('li'):
-        for full in song.find_all('li'):
-            if full.find('span', class_='projectNameType').find('strong', class_='fullProjectTextColor') is None:
-                continue
+	count = 1
+	flag = 0
+	for song in soup.find_all('li'):
+		artist = song.find('span',class_ = 'artistHeader')
+		if artist == None:
+			continue
+		else:
+			artist_name = artist.text
+			if artist_name == "Arise: 'Run Run Run'":
+				flag = 1
+		if flag == 1:
+			for full in song.find_all('li'):
+				if full.find('span', class_='projectNameType').find('strong', class_='fullProjectTextColor') is None:
+					continue
 
-            datapoint_dir_name = ''.join(['song_', str(count)])
-            datapoint_dir = os.path.join(save_dir_abs, datapoint_dir_name)
-            if not os.path.exists(datapoint_dir):
-                os.makedirs(datapoint_dir)
+				datapoint_dir_name = ''.join(['song_', str(count)])
+				datapoint_dir = os.path.join(save_dir_abs, datapoint_dir_name)
+				if not os.path.exists(datapoint_dir):
+					os.makedirs(datapoint_dir)
 
-            zip_html = full.find_all('span', class_='projectLine')[1]
-            zip_file_link = zip_html.find('a', href=True)['href']
-            zip_name = zip_file_link.split('/')[-1]
-            save_file_path_zip = os.path.join(datapoint_dir, zip_name)
+				zip_html = full.find_all('span', class_='projectLine')[1]
+				zip_file_link = zip_html.find('a', href=True)['href']
+				zip_name = zip_file_link.split('/')[-1]
+				save_file_path_zip = os.path.join(datapoint_dir, zip_name)
 
-            mixed_file_link = full.find_all('span', class_='projectLine')[0].find('a', href=True)['href']
-            mixed_file_name = mixed_file_link.split('/')[-1]
-            save_file_path_mixed = os.path.join(datapoint_dir, mixed_file_name)
+				mixed_file_link = full.find_all('span', class_='projectLine')[0].find('a', href=True)['href']
+				mixed_file_name = mixed_file_link.split('/')[-1]
+				save_file_path_mixed = os.path.join(datapoint_dir, mixed_file_name)
 
-            name = zip_name.split('.zip')[0]
-            print "----------------------------------------------------------------------------------------------------"
-            print "{} FILE NAME : {}".format(count, name)
-            print datapoint_dir
+				name = zip_name.split('.zip')[0]
+				print "----------------------------------------------------------------------------------------------------"
+				print "{} FILE NAME : {}".format(count, name)
+				print datapoint_dir
 
-            try:
-                download_file(mixed_file_link, save_file_path_mixed)
-                download_file(zip_file_link, save_file_path_zip)
-            except Exception as e:
-                import traceback
-                error = traceback.format_exc()
-                print "FAILED -- FILE NAME :", name
-                print error
-            count += 1
+				try:
+					download_file(mixed_file_link, save_file_path_mixed)
+					download_file(zip_file_link, save_file_path_zip)
+				except Exception as e:
+					import traceback
+					error = traceback.format_exc()
+					print "FAILED -- FILE NAME :", name
+					print error
+				count += 1
 
 
 if __name__ == '__main__':
-    cambridge_urls = ['http://cambridge-mt.com/ms-mtk.htm#AltRock', 'http://cambridge-mt.com/ms-mtk.htm#Rock']
-    save_dir = '../dataset_raw'
-    for download_url in cambridge_urls:
-        download_songs_from_cambridge_mt(download_url, save_dir)
+	cambridge_urls = ['http://cambridge-mt.com/ms-mtk.htm#AltRock']
+	save_dir = '../dataset_raw'
+	for download_url in cambridge_urls:
+		download_songs_from_cambridge_mt(download_url, save_dir)
