2343654b959d806e8c53679defbb9f7c1cf81623
T.Aditya <T.Aditya@iiitb.org>
2019-02-12 19:01:53 +0000

    Updates packages.sh with ffmpeg

--- packages.sh
+++ packages.sh
@@ -2,6 +2,10 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # for pysoundfile in ubuntu based operating systems
+    sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+    sudo apt-get update
+    sudo apt-get update
+    ffmpeg -version
     sudo apt install libsndfile1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     if [[ "$(which brew)" != "brew not found" ]]; then
